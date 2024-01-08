5a949704b67dea2b5a6a90b76d3aeca3c1da7a15
Marek <102665033+Matus4709@users.noreply.github.com>
2024-01-08 09:41:20 +0100

    django-simple-todolist-mysql-rawqueries

---
+++ todolistmysql/todolist/urls.py
@@ -0,0 +1,19 @@
+from todolist import views
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+app_name = 'todolist'
+
+urlpatterns = [
+    path('',views.IndexView.as_view(),name='index'),
+    path('accounts/profile/',views.IndexView.as_view(),name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='index.html'),name='logout'),
+    path('signup/',views.SingUp.as_view(),name='signup'),
+    path('add/', views.create_task, name='add'),
+    path('task_list/', views.task_list, name='task_list'),
+    path('task_list/<int:task_id>/make_completed/', views.task_completed,name='task_completed'),
+    path('task_list/<int:task_id>/delete_task/', views.delete_task,name='delete_task'),
+    path('task_list/<int:task_id>/edit/', views.TaskEditView.as_view(), name='edit_task'),
+    
+]
---
+++ todolistmysql/todolist/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Task
+from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import (TemplateView, CreateView, UpdateView)
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from todolist import forms
+from django.db import connection
+from todolist.forms import TaskForm
+from django.utils.decorators import method_decorator
+# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+class SingUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('todolist:login')
+    template_name = 'signup.html'
+    
+class LoginView(LoginView):
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('todolist:index') 
+    
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+
+@login_required
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            print(task)
+            return redirect('todolist:task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'add.html', {'form': form})
+
+def task_list(request):
+    # tasks = Task.objects.filter(user=request.user)  # Pobierz zadania dla zalogowanego użytkownika
+     
+    with connection.cursor() as cursor:
+        user_id = request.user.id
+        cursor.execute("SELECT * FROM todolist_task WHERE user_id = %s", [user_id])
+        tasks = cursor.fetchall()
+    
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+@login_required
+def task_completed(request,task_id):
+    with connection.cursor() as cursor:
+          cursor.execute("UPDATE todolist_task SET completed='1' WHERE id=%s" ,[task_id])
+          
+    with connection.cursor() as cursor:
+        user_id = request.user.id
+        cursor.execute("SELECT * FROM todolist_task WHERE user_id = %s", [user_id])
+        tasks = cursor.fetchall()
+    return render(request, 'task_list.html',{'tasks': tasks})
+
+@login_required
+def delete_task(request,task_id):
+    with connection.cursor() as cursor:
+          cursor.execute("DELETE FROM todolist_task WHERE id=%s" ,[task_id])
+    
+    with connection.cursor() as cursor:
+        user_id = request.user.id
+        cursor.execute("SELECT * FROM todolist_task WHERE user_id = %s", [user_id])
+        tasks = cursor.fetchall()
+    return render(request, 'task_list.html',{'tasks': tasks})
+
+@method_decorator(login_required, name='dispatch')
+class TaskEditView(UpdateView):
+    model = Task
+    fields = ['title', 'completed']  # Fields to be edited
+    template_name = 'edit_task.html'  # Template for editing
+    success_url = '/task_list/'  # URL to redirect after successful update
+    
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get('task_id')  # Retrieve the pk from URL parameters
+        return Task.objects.get(pk=pk)
